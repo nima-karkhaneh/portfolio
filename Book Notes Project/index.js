893721156c77552db68ecc03dfd8b9ac2f7ca593
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2024-12-16 22:37:50 +1100

    adds initial project files

---
+++ Book Notes Project/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import axios from "axios";
+
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", (req,res)=>{
+    res.render("index.ejs")
+})
+
+
+
+app.listen(port, () =>{
+    console.log(`server is listening on http://localhost:3000/`);
+})
---
+++ Book Notes Project/public/script.js
@@ -0,0 +1,3 @@
+const date = new Date ();
+const year = date.getFullYear();
+document.querySelector("footer").innerHTML =`Copyright &copy; ${year} - Website created by Nima Karkhaneh`
