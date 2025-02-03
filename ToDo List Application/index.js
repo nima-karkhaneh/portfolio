ff3bf7a9de27006e2783072dcf4a6edbbbd0b792
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-02-04 00:18:37 +1100

    completes the API routes

--- ToDo List Application/index.js
+++ ToDo List Application/index.js
@@ -19,19 +19,64 @@ const db = new pg.Client({
 });
 db.connect();
 
-// ROUTES
+// API ROUTES
+
+// GET ROUTE
+app.get("/todos", async (req, res) => {
+    try{
+        const item = await db.query("SELECT * FROM items ORDER BY id ASC")
+        res.json(item.rows)
+    }
+    catch(err){
+        console.error(err.message)
+    }
+})
 
 // POST ROUTE
 app.post("/submit", async (req, res) => {
     const { description } = req.body;
     console.log(description)
     try{
-        await db.query("INSERT INTO items (description) VALUES ($1);", [description])
+         const addItem = await db.query("INSERT INTO items (description) VALUES ($1) RETURNING *;", [description])
+        res.json(addItem.rows[0])
     }
+
     catch(err){
         console.error(err.message)
     }
 
+})
+
+// GET A SPECIFIC ITEM
+app.get("/todos/:id", async (req, res) => {
+    try{
+        const { id }= req.params
+        const selectedItem = await db.query("SELECT * FROM items WHERE id = ($1);", [id])
+        res.json(selectedItem.rows)
+    }
+    catch(err){
+        console.error(err.message)
+    }
+})
+
+// UPDATE AN ITEM
+app.put("/todos/:id", async (req, res) => {
+    try{
+        const { description } = req.body
+        const { id } = req.params
+        const selectedItem = await db.query("UPDATE items SET description = ($1) WHERE id = ($2)", [description, id]);
+        res.json("Item has been updated successfully!")
+    }
+    catch(err){
+        console.error(err)
+    }
+})
+
+// DELETE AN ITEM
+app.delete("/todos/:id", async (req, res) => {
+    const { id } = req.params;
+    const deletedItem = await db.query("DELETE FROM items WHERE id = ($1)", [id]);
+    res.json("Item was deleted!")
 })
 
 app.listen(PORT, () => {
