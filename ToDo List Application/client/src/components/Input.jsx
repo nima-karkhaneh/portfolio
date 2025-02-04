4c9d9287aabbf15ea90ae82ffa364bda8c7451a2
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-02-04 23:45:49 +1100

    sets up the post request with axios

--- ToDo List Application/client/src/components/Input.jsx
+++ ToDo List Application/client/src/components/Input.jsx
@@ -1,21 +1,28 @@
 import React, {useState} from "react";
+import axios from "axios";
 
 function Input() {
-    const [description, setDescription] = useState("")
+    const [inputText, setInputText] = useState("")
     function handleChange(e) {
         const {value} = e.target;
-        setDescription(value)
+        setInputText(value)
     }
-
     async function handleClick(e) {
         e.preventDefault();
+        try{
+            const response = await axios.post("http://localhost:3000/submit", {description:inputText});
+            console.log(response.data)
+        }
+        catch(err){
+            console.log(err)
+        }
     }
     return(
         <>
         <div className="container">
             <h1>Daily ToDo List</h1>
             <form>
-                <input className="form-control" type="text" placeholder="Add your ToDo Item" onChange={handleChange} value={description}/>
+                <input className="form-control" type="text" placeholder="Add your ToDo Item" onChange={handleChange} value={inputText}/>
                 <button type="submit" className="btn btn-outline-secondary" onClick={handleClick}>Add</button>
             </form>
         </div>
