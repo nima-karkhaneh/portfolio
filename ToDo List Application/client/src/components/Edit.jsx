ea9225222ce02123bf55311709461140a207a926
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-02-10 17:00:13 +1100

    finishes the edit button functionality

--- ToDo List Application/client/src/components/Edit.jsx
+++ ToDo List Application/client/src/components/Edit.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import axios from "axios";
 
 
 function Edit({item}) {
@@ -8,19 +9,31 @@ function Edit({item}) {
         const { value } = e.target
         setEditItem(value)
     }
+
+    async function updateItem(){
+        try{
+            const API_URL_PUT=import.meta.env.VITE_API_URL_PUT
+            await axios.put(`${API_URL_PUT}${item.id}`,{description:editItem});
+            window.location = ("/")
+        }
+        catch(err){
+            console.log(err.message)
+        }
+
+    }
     return(
         <>
             <button type="button" className="btn btn-warning" data-bs-toggle="modal" data-bs-target={`#id${item.id}`}>
                 Edit
             </button>
 
-            <div className="modal" id={`id${item.id}`}>
+            <div className="modal" id={`id${item.id}`} onClick={() => setEditItem(item.description)}>
                 <div className="modal-dialog">
                     <div className="modal-content">
 
                         <div className="modal-header">
                             <h4 className="modal-title">Edit your Todo Item</h4>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" onClick={() => setEditItem(item.description)}></button>
                         </div>
 
                         <div className="modal-body">
@@ -28,7 +41,7 @@ function Edit({item}) {
                         </div>
 
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-success" data-bs-dismiss="modal">Submit</button>
+                            <button type="button" className="btn btn-success" onClick={updateItem} data-bs-dismiss="modal">Submit</button>
                         </div>
 
                     </div>
