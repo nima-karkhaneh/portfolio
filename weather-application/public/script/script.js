1728daa008794c933802f49316e84a1bd561b844
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-08-04 21:10:35 +1000

    Finalise form validation in a separate script.js file

--- weather-application/public/script/script.js
+++ weather-application/public/script/script.js
@@ -3,3 +3,15 @@ const error = input.dataset.error;
 if (error) {
     input.placeholder = error
 }
+
+
+document.querySelector("form").addEventListener("submit", (e) => {
+    const value = input.value.trim()
+    const valid = /^[A-Za-z\s\-']{2,50}/.test(value);
+
+    if (!valid) {
+        e.preventDefault();
+        input.value= "";
+        input.placeholder= "Enter a valid Australian town name"
+    }
+})
