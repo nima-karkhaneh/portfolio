2498aa9de37c6fedc03ad751bedd2ea47b7d3d8c
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-08-06 02:14:22 +1000

    fix: Correctly calculate daily min and max temp using local date

--- weather-application/index.js
+++ weather-application/index.js
@@ -28,13 +28,28 @@ app.post("/submit", async (req, res) =>{
     try{
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},AU&appid=${key}&units=metric`);
         const result = response.data;
+        const { lon, lat } = result.coord;
+        const forecastRes = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=metric`);
+        const today = new Date().toLocaleDateString("sv-SE"); // Gives "YYYY-MM-DD" in your local time
+
+
+        const todayTemps = forecastRes.data.list
+            .filter(entry => entry.dt_txt.startsWith(today))
+            .map(entry => entry.main.temp)
+            .slice(0,8);
+
+        const tempMin = Math.min(...todayTemps);
+        const tempMax = Math.max(...todayTemps);
+
+
+
         const data = {
             description: result.weather[0].main,
             icon: result.weather[0].icon,
             city: result.name,
             temp: Math.round(result.main.temp),
-            tempMin: Math.floor(result.main.temp_min),
-            tempMax: Math.round(result.main.temp_max),
+            tempMin: Math.round(tempMin),
+            tempMax: Math.round(tempMax),
             humidity: result.main.humidity
         }
         res.render("index.ejs", data);
@@ -44,16 +59,18 @@ app.post("/submit", async (req, res) =>{
 
         if (err.response) {
             // The API responded but with an error (e.g., 404 for city not found)
+            console.error(`400 | ${err.message}`)
             errMsg = "Town has to be in Australia";
             res.status(400).render("index.ejs", { errMsg });
         } else if (err.request) {
             // The request was made but no response received (e.g., API server down)
             errMsg = "Unable to connect to weather service";
+            console.error(`503 | ${err.message}`)
             res.status(503).render("index.ejs", { errMsg });
         } else {
             // Unexpected backend error
             errMsg = "Something went wrong";
-            console.error("Unexpected error:", err.message);
+            console.error(`500 | Unexpected error:, ${err.message}`);
             res.status(500).render("index.ejs", { errMsg });
         }
     }
