a2174c8fd14def7b9ff6d3e64210b28bd7649226
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-08-12 13:53:33 +1000

    Add post route to restful api, drop rate from library table

--- book-notes-project/backend/api.js
+++ book-notes-project/backend/api.js
@@ -7,6 +7,8 @@ const app = express();
 const port = process.env.API_PORT || 4000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/books", async (req, res) => {
     try {
         const sort = req.query.sort;
@@ -26,7 +28,15 @@ app.get("/books", async (req, res) => {
         }
 
         const queryText = `
-              SELECT library.id, library.title, library.author, library.isbn, library.date, library.review, rating.rate
+              SELECT 
+                library.id,
+                library.title,
+                library.author,
+                library.isbn,
+                library.reader_name,
+                library.date,
+                library.review,
+                rating.rate
               FROM library
               LEFT JOIN rating ON library.id = rating.library_id
               ORDER BY ${orderBy};
@@ -45,4 +55,48 @@ app.get("/books", async (req, res) => {
         console.log(err.message);
         res.status(500).json({ error: "server error" })
     }
+})
+
+
+app.post ("/submit", async (req, res) => {
+    const { title, author, ISBN, date, review, rate, reader_name } = req.body;
+
+    try  {
+        const checkBook = await db.query("SELECT * FROM library WHERE isbn = $1", [ISBN]);
+        if (checkBook.rows.length !== 0) {
+            return res.status(400).json({ error: "Book already exists in the library." })
+        }
+
+        const insertLibraryQuery = `
+      INSERT INTO library (title, author, isbn, date, review, reader_name)
+      VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING id;
+    `;
+        const libraryResult = await db.query(insertLibraryQuery, [title, author, ISBN, date, review, reader_name]);
+
+        const newLibraryId = libraryResult.rows[0].id;
+
+        const insertRatingQuery = `
+        INSERT INTO rating (rate, library_id) VALUES ($1, $2);
+    `;
+
+        await db.query(insertRatingQuery, [rate, newLibraryId]);
+        res.status(201).json( {message: "Book added successfully."})
+
+    }
+
+    catch (err) {
+        console.log(err.message);
+        res.status(500).json( { error: "server error" });
+    }
+})
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`server is listening on http://localhost:${port}/`);
 })
