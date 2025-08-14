2f2b02acd454200d9fc4553323d87516b978dca2
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-08-14 19:16:07 +1000

    WIP: Setup /books route in server.js

--- book-notes-project/backend/server.js
+++ book-notes-project/backend/server.js
@@ -5,6 +5,7 @@ env.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const API_BASE_URL = process.env.API_BASE_URL
 
 app.use(express.static("public"));
 
@@ -15,6 +16,32 @@ app.get("/", (req,res)=>{
 })
 app.get("/add", (req,res)=>{
     res.render("add.ejs");
+})
+
+app.get("/books", async(req, res) => {
+    try {
+        const response = await axios.get(`${API_BASE_URL}/books`)
+        // checks for undefined or null values at each level
+        const booksArr = response?.data?.books ?? [];
+
+
+        // Implicit return using parentheses
+        const starDisplay = booksArr.map(book => ({
+            // Use ?? instead of || so that valid 0 ratings are preserved.
+            // ?? only falls back if value is null or undefined, unlike || which treats 0 as falsy.
+            library_id: book.id,
+            rate: book?.rate ?? 0
+        }))
+
+        res.render("books.ejs", {
+            booksArr: booksArr,
+            starDisplay: starDisplay
+        })
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Server error. Unable to fetch data" })
+    }
 })
 
 
