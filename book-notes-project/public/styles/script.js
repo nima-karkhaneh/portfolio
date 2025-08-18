6436acd815a6624b44def5e05eae8b9b63815872
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-08-18 18:00:27 +1000

    Add reader_name to book.ejs, style accrodingly

--- book-notes-project/public/styles/script.js
+++ book-notes-project/public/styles/script.js
@@ -1,6 +1,10 @@
 const date = new Date ();
 const year = date.getFullYear();
 document.querySelector("footer").innerHTML = `<p>Copyright &copy; ${year} - Website created by Nima Karkhaneh</p><p><i class=\"fa-solid fa-envelope\"></i>nimakarkahneh@gmail.com</p><p><a href=\"/\"><i class=\"fab fa-linkedin\"></a></i><a href=\"/\"><i class=\"fa-brands fa-github\"></i></a></p>`;
+
+
+// Star rating functionality on add.ejs & edit.ejs
+
 const stars = document.querySelectorAll(".star-ranking");
 stars.forEach((star, index1) =>{
     star.addEventListener("click", ()=>{
