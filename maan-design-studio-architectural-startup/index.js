9085545378f6ac931f8f377c45a7043fa44c4407
Nima Karkhaneh <nimakarkhaneh@gmail.com>
2025-11-07 20:41:34 +1100

    Add port binding to server

--- maan-design-studio-architectural-startup/index.js
+++ maan-design-studio-architectural-startup/index.js
@@ -112,10 +112,6 @@ app.use((req, res) => {
 })
 
 
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(port, () => {
-        console.log(`Server running at http://localhost:${port}`);
-    });
-} else {
-    app.listen(port);
-}
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port}`);
+});
